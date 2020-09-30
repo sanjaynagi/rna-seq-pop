7bdef9ce12a5f77c6ece052354532459d62dd773
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2020-09-30 15:34:08 +0100

    deleting old files
    Former-commit-id: ffa713217df4d425c4c43ede26c73ab8a91d8ee3

--- workflow/scripts/functional_enrichment.R
+++ workflow/scripts/functional_enrichment.R
@@ -7,8 +7,11 @@ library(gage)
 library(KEGGREST)
 library(clusterProfiler)
 library(fgsea)
+library(data.table)
+library(glue)
+library(tidyverse)
+  
 
-rm(list=ls())
 ############## GSEA ################
 
 samples = fread("config/samples.tsv") %>% as.data.frame()
@@ -24,13 +27,28 @@ diff = de %>% select('absolute_diff')
 
 
 
+######
+data(examplePathways)
+data(exampleRanks)
 
 
 
+fgseaRes <- fgsea(pathways = examplePathways, 
+                  stats    = exampleRanks,
+                  minSize  = 15,
+                  maxSize  = 500,
+                  nperm = 10000)
 
 
 
+plotEnrichment(examplePathways[["5991130_Programmed_Cell_Death"]],
+               exampleRanks) + labs(title="Programmed Cell Death")
 
+topPathwaysUp <- fgseaRes[ES > 0][head(order(pval), n=10), pathway]
+topPathwaysDown <- fgseaRes[ES < 0][head(order(pval), n=10), pathway]
+topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
+plotGseaTable(examplePathways[topPathways], exampleRanks, fgseaRes, 
+              gseaParam=0.5)
 
 
 
