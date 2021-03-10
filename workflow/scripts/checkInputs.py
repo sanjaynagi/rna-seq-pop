84056a8a2dad12d052416e2d45fac1c1d07bc044
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2021-03-10 16:49:26 +0000

    check inputs add script
    Former-commit-id: 10307f6ebd631f174d0389a0101e2bb6e2df08b5

---
+++ workflow/scripts/checkInputs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+A script to check the validity of input files and parameters to the workflow
+"""
+
+import sys
+sys.stderr = open(snakemake.log[0], "w")
+
+from tools import *
+
+metadata = pd.read_csv(snakemake.params['metadata'], sep="\t")
+gffpath = snakemake.params['gffpath']
+chroms = snakemake.params['chroms']
+
+# Check to see if fastq files match the metadata
+for sample in metadata.samples:
+    for n in [1,2]:
+        fqpath = f"resources/reads/{sample}_{n}.fastq.gz"
+        assert os.path.isfile(fqpath), f"all sample names in 'samples.tsv' do not match a .fastq.gz file in the {snakemake.workflow.basedir}/resources/reads/ directory"
+
+# Check that the chromosomes are present in the gff file 
+gff = allel.gff3_to_dataframe(gffpath)
+assert np.isin(chroms, gff.seqid).all(), f"All provided chromosome names ({chroms}) are not present in GFF file"
+
+# Check column names of gene_names.tsv
+gene_names = pd.read_csv(snakemake.params['gene_names'], sep="\t")
+colnames = ['Gene_stable_ID', 'Gene_description', 'Gene_name']
+
+assert (gene_names.columns == colnames).all(), f"Column names of gene_names.tsv should be {colnames}"
