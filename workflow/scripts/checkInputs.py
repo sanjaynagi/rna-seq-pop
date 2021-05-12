9a69f1c4e50d58d9a40cb113fb17afa407e9ebfc
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2021-05-12 11:51:39 +0100

    edit checkInputs
    Former-commit-id: 6446353ceb59e2ea02680b5eab3bcbc30a0f3411

--- workflow/scripts/checkInputs.py
+++ workflow/scripts/checkInputs.py
@@ -15,12 +15,6 @@ gffpath = snakemake.params['gffpath']
 chroms = snakemake.params['chroms']
 autofastq = snakemake.params['fastq']
 
-# Check to see if fastq files match the metadata
-for sample in metadata.samples:
-    for n in [1,2]:
-        fqpath = f"resources/reads/{sample}_{n}.fastq.gz"
-        assert os.path.isfile(fqpath), f"all sample names in 'samples.tsv' do not match a .fastq.gz file in the {snakemake.workflow.basedir}/resources/reads/ directory"
-
 # Check that the chromosomes are present in the gff file 
 #gff = allel.gff3_to_dataframe(gffpath)
 #assert np.isin(chroms, gff.seqid).all(), f"All provided chromosome names ({chroms}) are not present in GFF file"
@@ -34,7 +28,14 @@ assert (np.isin(comparisons, metadata['treatment']).all()), f"treatments specifi
 # Check that samples in fastq.tsv match those in metadata.tsv
 if autofastq is False:
     fastq = pd.read_csv(snakemake.params['table'], sep="\t")
-    assert np.isin(fastq['samples'], metadata['samples']).all(), f"all samples specified in fastq table do not match those in samples.tsv"
+    assert np.isin(fastq['samples'], metadata['samples']).all(), f"all samples specified in fastq table do not match those in samples.tsv, please check your fastq.tsv file"
+else:
+        # Check to see if fastq files match the metadata
+    for sample in metadata['samples']:
+        for n in [1,2]:
+            fqpath = f"resources/reads/{sample}_{n}.fastq.gz"
+            assert os.path.isfile(fqpath), f"all sample names in 'samples.tsv' do not match a .fastq.gz file in the {snakemake.workflow.basedir}/resources/reads/ directory, please rename or consider using the fastq table option"
+
 
 # Check column names of gene_names.tsv
 gene_names = pd.read_csv(snakemake.params['gene_names'], sep="\t")
