901f21c611a398891153a5b6af43ec76d7b5aeb3
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2022-01-10 15:20:40 +0000

    KaryoPlots add
    Former-commit-id: 594e47c4f877c1f0bd1de42eb46dbee49acfd729

--- workflow/scripts/KaryoPlots.py
+++ workflow/scripts/KaryoPlots.py
@@ -14,11 +14,13 @@ ploidy = snakemake.params['ploidy']
 invs = snakemake.params['inversions']
 metadata = pd.read_csv(snakemake.input['metadata'], sep="\t")
 metadata = metadata.sort_values(by='species')
-
+dataset = snakemake.params['dataset']
 ##
+
+
 karyo = {}
 for inv in invs:
-    df = pd.read_csv(f"../../results/karyotype/{inv}.karyo.txt", sep="\s+", header=None)
+    df = pd.read_csv(f"../../results/karyotype/{inv}.{dataset}.karyo.txt", sep="\s+", header=None)
     df = df.rename(columns={0:'Sample', 1:'KaryoScore', 2:'n_SNPtags'})
     df[inv] = df['KaryoScore']/ploidy
     df.rename({inv:f'{inv} frequency'}).to_csv(f"../../results/karyotype/{inv}.karyo.txt", sep="\t")
@@ -37,4 +39,4 @@ plotRectangular(karyo, path="results/karyotype/karyoFreqs.png" , cmap='mako_r', 
 df = karyo.T.reset_index()
 df['Sample'] = df['Sample'].str.strip('12345678910')
 df = df.groupby("Sample").agg('mean').T
-plotRectangular(df, path="../../results/karyotype/karyoOverallFreqs.png", ylab='Inversion', cbar=False, figsize=[2,len(invs)])
+plotRectangular(df, path="results/karyotype/karyoOverallFreqs.png", ylab='Inversion', cbar=False, figsize=[2,len(invs)])
